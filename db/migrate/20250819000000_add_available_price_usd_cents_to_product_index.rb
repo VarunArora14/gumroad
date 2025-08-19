df566f386ed6d4e2e3139bf5b7cd84ea5c31e22e
abbbshek <wadekarabhishek2003@gmail.com>
2025-08-19 15:29:48 +0000

    fix discover price sorting issue

---
+++ db/migrate/20250819000000_add_available_price_usd_cents_to_product_index.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+class AddAvailablePriceUsdCentsToProductIndex < ActiveRecord::Migration[6.1]
+  def up
+    EsClient.indices.put_mapping(
+      index: Link.index_name,
+      body: {
+        properties: {
+          available_price_usd_cents: { type: "long" }
+        }
+      }
+    )
+  end
+end
+
+
