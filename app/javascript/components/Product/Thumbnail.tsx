9b86823172cd241b2f8d72bf7d8cb67f80baca1b
Hanaffi <ahmedellord56@gmail.com>
2025-08-28 22:44:39 +0300

    Enable lazy loading for offscreen images across various components

--- app/javascript/components/Product/Thumbnail.tsx
+++ app/javascript/components/Product/Thumbnail.tsx
@@ -3,7 +3,31 @@ import { cast } from "ts-safe-cast";
 
 import { ProductNativeType } from "$app/parsers/product";
 
+import { useLoggedInUser } from "$app/components/LoggedInUser";
+
 const nativeTypeThumbnails = require.context("$assets/images/native_types/thumbnails/");
 
-export const Thumbnail = ({ url, nativeType }: { url: string | null; nativeType: ProductNativeType }) =>
-  url ? <img src={url} /> : <img src={cast(nativeTypeThumbnails(`./${nativeType}.svg`))} />;
+export const Thumbnail = ({
+  url,
+  nativeType,
+  eager,
+}: {
+  url: string | null;
+  nativeType: ProductNativeType;
+  eager?: boolean | undefined;
+}) => {
+  const loggedInUser = useLoggedInUser();
+  const commonProps: React.ImgHTMLAttributes<HTMLImageElement> = {
+    ...(eager == null || !loggedInUser?.lazyLoadOffscreenDiscoverImages
+      ? {}
+      : {
+          fetchpriority: eager ? "high" : "auto",
+          loading: eager ? ("eager" as const) : ("lazy" as const),
+        }),
+  };
+  return url ? (
+    <img src={url} {...commonProps} />
+  ) : (
+    <img src={cast(nativeTypeThumbnails(`./${nativeType}.svg`))} {...commonProps} />
+  );
+};
--- app/javascript/components/Wishlist/Card.tsx
+++ app/javascript/components/Wishlist/Card.tsx
@@ -6,6 +6,7 @@ import { formatOrderOfMagnitude } from "$app/utils/formatOrderOfMagnitude";
 import { getCssVariable } from "$app/utils/styles";
 
 import { Icon } from "$app/components/Icons";
+import { useLoggedInUser } from "$app/components/LoggedInUser";
 import { AuthorByline } from "$app/components/Product/AuthorByline";
 import { useFollowWishlist } from "$app/components/Wishlist/FollowButton";
 
@@ -24,12 +25,21 @@ export type CardWishlist = {
   can_follow: boolean;
 };
 
-export const Card = ({ wishlist, hideSeller }: { wishlist: CardWishlist; hideSeller?: boolean }) => {
+export const Card = ({
+  wishlist,
+  hideSeller,
+  eager,
+}: {
+  wishlist: CardWishlist;
+  hideSeller?: boolean;
+  eager?: boolean;
+}) => {
   const { isFollowing, isLoading, toggleFollowing } = useFollowWishlist({
     wishlistId: wishlist.id,
     wishlistName: wishlist.name,
     initialValue: wishlist.following,
   });
+  const loggedInUser = useLoggedInUser();
 
   const thumbnailUrl = wishlist.thumbnails.find((thumbnail) => thumbnail.url)?.url;
   const [backgroundColor, setBackgroundColor] = React.useState<string>(thumbnailUrl ? "transparent" : "var(--pink)");
@@ -70,6 +80,12 @@ export const Card = ({ wishlist, hideSeller }: { wishlist: CardWishlist; hideSel
             src={url ?? cast(nativeTypeThumbnails(`./${native_type}.svg`))}
             role="presentation"
             crossOrigin="anonymous"
+            {...(eager == null || !loggedInUser?.lazyLoadOffscreenDiscoverImages
+              ? {}
+              : {
+                  loading: eager ? ("eager" as const) : ("lazy" as const),
+                  fetchpriority: eager ? "high" : "auto",
+                })}
           />
         ))}
         {wishlist.thumbnails.length === 0 ? <img role="presentation" /> : null}
